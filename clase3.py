79cd7c4fa10814bfd6a55775db6973d1d3eaefb8
Matiaslopezmun <Mat.lopezm@duocuc.cl>
2026-04-20 09:10:40 -0400

    Pokemones

---
+++ clase3.py
@@ -0,0 +1,14 @@
+###POKEMONES###
+entrenador="aaa"
+pkm1=6
+
+entrenador2="bbb"
+pkm2=7
+
+if pkm1<=6:
+    print("Puede acceder a la lisa pokemon")
+else:
+    print("Tiene mas pokemons de los necesarios")
+
+
+
