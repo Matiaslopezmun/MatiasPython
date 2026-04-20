74fc776dee21474f144a71555a2cd45eb307f50d
Matiaslopezmun <Mat.lopezm@duocuc.cl>
2026-04-20 09:40:21 -0400

    concatenaciones

--- clase3.py
+++ clase3.py
@@ -1,14 +1,41 @@
 ###POKEMONES###
+
 entrenador="aaa"
 pkm1=6
 
-entrenador2="bbb"
+entrenador2="bbbb"
 pkm2=7
 
-if pkm1<=6:
-    print("Puede acceder a la lisa pokemon")
-else:
-    print("Tiene mas pokemons de los necesarios")
+# if pkm1<=6:
+#     print("Puede acceder a la lisa pokemon")
+# else:
+#     print("Tiene mas pokemons de los necesarios")
 
 
+# pkm1=int(input("Ingrese la cantidad de pokemones: "))
+
+# if pkm1<=6:
+#     print("Puede acceder a la lisa pokemon")
+# else:
+#     print("Tiene mas pokemons de los necesarios")
+
+# pkm2=int(input("Ingrese la cantidad de pokemones: "))
+
+# if pkm2<=6:
+#     print("Puede acceder a la lisa pokemon")
+# else:
+#     print("Tiene mas pokemons de los necesarios")
+
+
+# print(len(entrenador))
+
+# if len(entrenador)>=5:
+#     print("Correcto")
+# else:
+#     print("incorrecto")
+
+
+# print("El entrenador", entrenador, "tiene", pkm1, "pokemons")
+# print("El entrenador 2", entrenador2, "tiene", pkm2, "pokemons")
+# print(f"El entrenador 2 {entrenador2} tiene {pkm2} pokemons")
 
